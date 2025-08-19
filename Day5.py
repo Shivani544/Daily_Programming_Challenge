1352cfa7cc059807c7732b732f730e2ebffa69e2
Shivani Gire <160721702+Shivani544@users.noreply.github.com>
2025-08-19 23:25:38 +0530

    Create Day5.py

---
+++ Day5.py
@@ -0,0 +1,14 @@
+def find_leaders(arr):
+    n = len(arr)
+    result = []
+    max_from_right = float('-inf')
+    seen = set()
+
+    for i in range(n - 1, -1, -1):
+        if arr[i] > max_from_right:
+            if arr[i] not in seen:
+                result.append(arr[i])
+                seen.add(arr[i])
+            max_from_right = arr[i]
+
+    return result[::-1] 
